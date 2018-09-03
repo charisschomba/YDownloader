4a87d094ee8e39dee0d5b4e86829808ce15b3cdf
andela <andela@andelas-MacBook-Pro-2.local>
2018-09-03 22:22:45 +0300

    Added mac script

---
+++ Mac/README.md
@@ -0,0 +1,95 @@
+# YDownloader
+YDownloader script downloads Youtube contents.
+The script downloads Audio,Video and the playlist.
+
+## For Mac Users
+
+## Requirements
+
+- [Python3](https://www.python.org/) (programming languag)
+- [Virtualenv](https://virtualenv.pypa.io/en/stable/)(To isolate script modules)
+
+## How to setup it up:
+
+1.sudo install python3
+
+2.pip install virtualenv
+
+Clone this repository:
+
+git clone  https://github.com/charisschomba/YDownloader.git
+
+cd YDownloader
+
+## Create a virtual environment in the root directory:
+
+virtualenv [name of virtualenv]
+
+## Activate the virtualenv:
+
+source [name of virtualenv]/bin/activate
+
+## On your terminal run:
+
+pip install -r requirements.txt
+
+to install the modules
+
+## On your terminal run:
+
+python YDownloader.py
+
+
+## How to use it.
+You can manually enter the url or copy it and the script detect the copied url.
+
+## The  script has five options
+## 1. Audio
+
+Downloads Audio
+First create a Music folder in Downloads
+Downloads/Music
+
+## 2. Video
+
+Downloads Video
+First create a Video folder in Downloads
+Downloads/Video
+
+## 3. Playlist
+
+The url must be a playlist url.
+First create a Playlist folder in Downloads
+Downloads/Playlist
+
+## 4. From text file video
+
+This option downloads videos from a text file
+
+## 5. From text file Audio
+
+This option downloads Audio from a text file
+
+## For option 4 and 5
+
+The first you choose option 4 or 5 without url_list in the download folder,
+the script will create one for you.
+
+you should have url_list.txt in /home/[user]/Downloads/
+
+for example /home/anonymous/Downloads/url_list.txt with a list of all urls.
+
+if you get certificate errors run the following command to update them
+/Applications/'Python version'/Install Certificates.command
+User your current python version
+/Applications/Python 3.6/Install Certificates.command
+
+
+
+
+
+
+
+
+
+
---
+++ YDownloader-Linux.py
@@ -0,0 +1,192 @@
+import pafy,sys,getpass
+import os,time,subprocess as sub
+import pyautogui as py
+import pyperclip
+import youtube_dl
+
+user = getpass.getuser()
+
+file ='/home/{}/Downloads/Urls.txt'.format(user)
+ulr_list_dir ='/home/{}/Downloads'.format(user)
+if os.path.isfile(file) == True:
+    pass
+else:
+    path="/home/{}/Downloads".format(user)
+    os.chdir(path)
+    with open('Urls.txt','w') as file:
+        file.write('These are songs downloaded with Y-Downloader\n')
+
+def playlistInfo(url):
+    playlist=pafy.get_playlist(url)
+    print("Title : " + playlist['title'])
+    print("Author: " + playlist['author'])
+    print("Items : " + str(len(playlist['items'])))
+
+def mp3(url):
+    path="/home/{}/Downloads/Music".format(user)
+    os.chdir(path)
+    songs=pafy.new(url)
+    song=songs.getbestaudio()
+    print (songs.title)
+    file=song.download()
+    file_split=file.split('.webm')
+    renamed = file_split[0] + '.mp3'
+    file_path = "/home/{}/Downloads/Music/{}".format(user,file)
+    new_file = "/home/{}/Downloads/Music/{}".format(user,renamed)
+    os.rename(file_path,new_file)
+    print('Download Completed ')
+
+def mp4(url):
+    path="/home/{}/Downloads/Video".format(user)
+    os.chdir(path)
+    songs=pafy.new(url)
+    song=songs.getbest(preftype="any")
+    print (songs.title)
+    file=song.download(quiet=False)
+    print('Download Completed ')
+
+def playlist(url):
+    plurl = url
+    playlist = pafy.get_playlist(url)
+    path="/home/{}/Downloads".format(user) +"/Playlist/{}".format(playlist['title'])
+    join_=os.path.join(path)
+    file ='/home{}/Downloads/Playlist'.format(user)
+    if  os.path.isdir(file) == True:
+        pass
+    else:
+        os.mkdir(file)
+    if  os.path.isdir(path) == True:
+        pass
+    else:
+        dpath=os.mkdir(join_)
+    os.chdir(join_)
+    playlistInfo(url)
+    with youtube_dl.YoutubeDL({}) as ydl:
+        ydl.download([url])
+
+def from_text_file():
+    os.chdir(ulr_list_dir)
+    file = "url_list"
+    file = file +'.txt'
+    if os.path.isfile(file) == True:
+        if os.stat("url_list.txt").st_size == 0:
+            print('url_list.txt is empty,please add Audio/Vedio links to continue ')
+            print('url_list.txt is in {}'.format(os.getcwd()))
+        else:
+            print('Downloading Videos from '+ os.path.join(os.getcwd()+'/url_list.txt'))
+            with open(file,'r') as urls:
+                for link in urls:
+                    try:
+                        mp4(link)
+                    except:
+                        print("An error occurred while downloading "+str(link))
+            print('Download completed')
+def from_text_file_mp3():
+    os.chdir(ulr_list_dir)
+    file = "url_list"
+    file = file +'.txt'
+    if os.path.isfile(file) == True:
+        if os.stat("url_list.txt").st_size == 0:
+            print('url_list.txt is empty,please add Audio/Vedio links to continue ')
+            print('url_list.txt is in {}'.format(os.getcwd()))
+        else:
+            print('Downloading Videos or Audios from '+ os.path.join(os.getcwd()+'/url_list.txt'))
+            with open(file,'r') as urls:
+                for link in urls:
+                    try:
+                        mp3(link)
+                    except:
+                        print("An error occurred while downloading "+str(link))
+            print('Download completed')
+
+    elif os.path.isfile(file) == False:
+        with open('url_list.txt','w') as file:
+            print('Please add download links to '+os.path.join(os.getcwd()+'/url_list.txt') )
+    else:
+        print('The file passed does not exist the current directory')
+        print('Please double check it')
+
+def save_url_(url):
+    path="/home/{}/Downloads".format(user)
+    os.chdir(path)
+    song=pafy.new(url)
+    Title=song.title
+    c=open('Urls.txt','r+')
+    if Title in c.read():
+        pass
+    else:
+        try:
+            c.write(Title + '\n')
+            c.write(url + '\n')
+            c.close()
+        except:
+            pass
+
+def saveUrl(url):
+    path="/home/{}/Downloads".format(user)
+    os.chdir(path)
+    playlist=pafy.get_playlist(url)
+    Title=playlist['title']
+    c=open('Urls.txt','r+')
+    if Title in c.read():
+        pass
+    else:
+        try:
+            c.write(Title + '\n')
+            c.write(url + '\n')
+            c.close()
+        except:
+            pass
+
+def downloadType():
+
+    type=py.confirm(text='Choose Download Type',buttons=['Audio','Video','Playlist',"From Text File Video","From Text File Audio"])
+
+    if type == "Audio":
+        save_url_(url)
+        mp3(url)
+
+    elif type == 'Video':
+        save_url_(url)
+        mp4(url)
+    elif type == 'Playlist':
+       saveUrl(url)
+       playlist(url)
+    elif type == "From Text File Video":
+        from_text_file()
+    elif type == "From Text File Audio":
+        from_text_file_mp3()
+    else:
+        main()
+
+print('-------------------------------------------------------------------------------------------------------')
+print('YDownloader')
+print('Created by Chariss')
+print('Beta Version')
+print('--------------------------------------------------------------------------------------------------------')
+print(" For automatic Download copy the url or 11 digit youtube video code to clipboard and click Download ")
+url =''
+def main():
+    while True:
+        response=py.confirm(text='Download or Exit',buttons=['Download','Exit'])
+        if response == 'Download':
+            global url
+            url=pyperclip.paste() or py.prompt(text="URL",title='Enter Url',default='')
+            try:
+                downloadType()
+            except:
+                print('An error occured when processing your download request.')
+                print("Please use a Valid URL or Confirm if your clipboard has the correct URL")
+                print("Make sure youtube-dl is in Y-Downloader Directory.")
+                print('Y-Downloader downloads youtube content only.')
+
+        elif response == 'Exit':
+            print('Thanks for using Y Downloader')
+            sys.exit()
+
+        else:
+            print('You did not Press Download or Exit' )
+
+
+if __name__ == '__main__':
+   main()
